ca61d002a1c0365d892ac6c45ccbd5b8cca20a20
jinxin <cs.jinxinhe@gmail.com>
2016-06-01 13:04:00 -0700

    update

--- constants.h
+++ constants.h
@@ -2,11 +2,25 @@
 #define CONSTANTS_H
 
 
+struct meta_string
+{
+	uint8_t const * buffer; 
+	size_t  length; 
+}
+
+struct AuthInfo
+{
+	struct meta_string *uid; 
+	struct meta_string *pwd; 
+	struct meta_string *domain; 
+}; 
+
+
 #ifdef __cplusplus 
 	extern "C" {
 #endif 
 	
-
+/* define your personal info here or use struct */
 #define UID ""
 #define PWD ""
 #define domain ""
--- util.c
+++ util.c
@@ -1,3 +1,27 @@
+#include <xml.h>
+
+#define STR(X)      #X
+
+
+const char* getPwdFromXML(uint8_t *source)  // dir to xml file 
+{
+    struct xml_document* document = xml_parse_document(source, strlen(source)); 
+
+    if (!document)
+    {
+        printf("Parse failed %s\n", STR(document)); 
+        exit(EXIT_FAILURE); 
+    }
+
+    struct xml_node * root = xml_document_root(document); 
+
+    struct xml_node* pwd_content = xml_node_content(root);
+    uint8_t *pwd = calloc(xml_string_length(pwd_content) + 1, sizeof(uint8_t)); 
+
+    xml_document_free(document, false); 
+    return pwd; 
+}
+
 
 
 
@@ -21,41 +45,45 @@ void base64(char *dbuf, char *buf128, int len)
     char cc = '\0';  
     memset(buf, 0, 256);  
     strcpy(buf, buf128);  
-    for(i = 1; i <= len/3; i++)  
+    
+    for (i = 1; i <= len/3; i++)  
     {  
-    tmp = buf+(i-1)*3;  
-    cc = tmp[2];  
-    tmp[2] = tmp[0];  
-    tmp[0] = cc;  
-    ddd = (struct data6 *)tmp;  
-    dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
-    dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
-    dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);  
-    dbuf[(i-1)*4+3] = con628((unsigned int)ddd->d4);  
+        tmp = buf+(i-1)*3;  
+        cc = tmp[2];  
+        tmp[2] = tmp[0];  
+        tmp[0] = cc;  
+        ddd = (struct data6 *)tmp;  
+        dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
+        dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
+        dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);  
+        dbuf[(i-1)*4+3] = con628((unsigned int)ddd->d4);  
     }  
-    if(len%3 == 1)  
+    if (len%3 == 1)  
     {  
-    tmp = buf+(i-1)*3;  
-    cc = tmp[2];  
-    tmp[2] = tmp[0];  
-    tmp[0] = cc;  
-    ddd = (struct data6 *)tmp;  
-    dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
-    dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
-    dbuf[(i-1)*4+2] = '=';  
-    dbuf[(i-1)*4+3] = '=';  
+        tmp = buf+(i-1)*3;  
+        cc = tmp[2];  
+        tmp[2] = tmp[0];  
+        tmp[0] = cc;  
+        ddd = (struct data6 *)tmp;  
+        dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
+        dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
+        dbuf[(i-1)*4+2] = '=';  
+        dbuf[(i-1)*4+3] = '=';  
     }  
-    if(len%3 == 2)  
+    if (len%3 == 2)  
     {  
-    tmp = buf+(i-1)*3;  
-    cc = tmp[2];  
-    tmp[2] = tmp[0];  
-    tmp[0] = cc;  
-    ddd = (struct data6 *)tmp;  
-    dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
-    dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
-    dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);  
-    dbuf[(i-1)*4+3] = '=';  
+        tmp = buf+(i-1)*3;  
+        cc = tmp[2];  
+        tmp[2] = tmp[0];  
+        tmp[0] = cc;  
+        ddd = (struct data6 *)tmp;  
+        dbuf[(i-1)*4+0] = con628((unsigned int)ddd->d1);  
+        dbuf[(i-1)*4+1] = con628((unsigned int)ddd->d2);  
+        dbuf[(i-1)*4+2] = con628((unsigned int)ddd->d3);  
+        dbuf[(i-1)*4+3] = '=';  
     }  
     return;  
 }  
+
+
+
